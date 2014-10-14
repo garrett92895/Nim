34b0b2d9e1364310a017d895273a48d59f52e1e0
TMartz <TMartz@student.neumont.edu>
2014-10-14 08:26:08 -0600

    fixes and refactoring

--- src/neumont/Nim.java
+++ src/neumont/Nim.java
@@ -5,26 +5,16 @@ import java.util.Scanner;
 
 public class Nim
 {
-	static Scanner scan;
-	static Game game;
-	static String row, amount, input;
-	static int computerGames, PlayerOneWins, PlayerTwoWins,
-	PlayerOneComputerWins, PlayerTwoComputerWins;
-	static boolean debugComputers, printComputerWins;
+	private static Scanner scan;
+	private static Game game;
+	private static String row, amount, input;
+	private static int computerGames;
 
 	public static void main(String[] args)
 	{
 		scan = new Scanner(System.in);
 		input = "Start";
 		game = new Game();
-		computerGames = 0;
-		PlayerOneWins = 0;
-		PlayerTwoWins = 0;
-		PlayerOneComputerWins = 0;
-		PlayerTwoComputerWins = 0;
-		debugComputers = false;
-		printComputerWins = false;
-		
 		StateLibrary.createAllStates();
 		Menu();
 	}
@@ -60,7 +50,7 @@ public class Nim
 			{
 				Iterator<State> stateIterator = StateLibrary.getStates();
 				State s = null;
-				while(stateIterator.hasNext())
+				while (stateIterator.hasNext())
 				{
 					s = stateIterator.next();
 					System.out.println(s.getOne() + " " + s.getTwo() + " " + s.getThree() + " " + s.getValue());
@@ -68,10 +58,10 @@ public class Nim
 			}
 			else if ("wins".equalsIgnoreCase(input))
 			{
-				System.out.println("Player One has won " + PlayerOneWins + " game(s).");
-				System.out.println("Player Two has won " + PlayerTwoWins + " game(s).");
-				System.out.println("Player One as a computer has won " + PlayerOneComputerWins + " game(s).");
-				System.out.println("Player Two as a computer has won " + PlayerTwoComputerWins + " game(s).");
+				System.out.println("Player One has won " + game.getPlayerOne().getWinCount() + " game(s).");
+				System.out.println("Player Two has won " + game.getPlayerTwo().getWinCount() + " game(s).");
+				System.out.println("Player One as a computer has won " + game.getPlayerOne().getWinAsComputerCount() + " game(s).");
+				System.out.println("Player Two as a computer has won " + game.getPlayerTwo().getWinAsComputerCount() + " game(s).");
 			}
 		}
 		System.out.println("Goodbye");
@@ -95,54 +85,21 @@ public class Nim
 	{
 
 		System.out.println("Good luck to both players!");
-		if (game.getPlayerOne().isComputer() && game.getPlayerTwo().isComputer())
+		if (game.bothPlayersAreComputers())
 		{
-			System.out.println("How many games would you like the computers to play?");
-			input = scan.nextLine();
-			try
-			{
-				computerGames = Integer.parseInt(input);
-			}
-			catch (Exception e)
-			{
-				try
-				{
-					if ("k".endsWith(input.toLowerCase()))
-					{
-						input = input.substring(0, input.length() - 1);
-						computerGames = Integer.parseInt(input);
-						computerGames *= 1000;
-					}
-					else if ("m".endsWith(input.toLowerCase()))
-					{
-						input = input.substring(0, input.length() - 1);
-						computerGames = Integer.parseInt(input);
-						computerGames *= 1000000;
-					}
-					else
-					{
-						System.out.println("Invalid input, defaulting to 1");
-						computerGames = 1;
-					}
-				}
-				catch (Exception ex)
-				{
-					System.out.println("Invalid input, defaulting to 1");
-					computerGames = 1;
-				}
-			}
+			getComputerGameCount();
 		}
 		do
 		{
-			game = new Game(game.getPlayerOne(), game.getPlayerTwo());
+			game.reset();
 			while (!game.gameEnded())
 			{
-				if (computerGames <= 0 || debugComputers)
+				if (computerGames <= 0)
 				{
 					System.out.println("It is " + game.getPlayerTurn() + "'s turn.");
 					printGame();
 				}
-				if (game.isPlayerComputer())
+				if (game.isCurrentPlayerComputer())
 				{
 					String[] sa = StateLibrary.getBestMove().split(":");
 					row = sa[0];
@@ -154,61 +111,91 @@ public class Nim
 					amount = amount();
 				}
 				game.take(row, amount);
-				if (computerGames <= 0 || debugComputers)
+				if (computerGames <= 0)
 				{
 					System.out.println(game.getPlayerTurn() + " takes " + amount + " from " + row + ".");
 				}
 				game.switchTurn();
 			}
+			addWins();
 			if (computerGames <= 0)
 			{
 				System.out.println("Congratulations to " + game.getPlayerTurn() + ", you win!");
-				if (game.isPlayerOneTurn())
-				{
-					if (game.getPlayerOne().isComputer())
-					{
-						PlayerOneComputerWins++;
-					}
-					else
-					{
-						PlayerOneWins++;
-					}
-				}
-				else
-				{
-					if (game.getPlayerTwo().isComputer())
-					{
-						PlayerTwoComputerWins++;
-					}
-					else
-					{
-						PlayerTwoWins++;
-					}
-				}
 			}
 			else
 			{
-				if (printComputerWins)
-				{
-					System.out.println("Congratulations to " + game.getPlayerTurn() + ", you win!");
-				}
 				System.out.println("Game finished. " + (computerGames - 1) + " more games to play.");
-				if (game.isPlayerOneTurn())
-				{
-
-					PlayerOneComputerWins++;
-				}
-				else
-				{
-					PlayerTwoComputerWins++;
-				}
 			}
 			StateLibrary.valueMoves(game.isPlayerOneTurn());
 			computerGames--;
 		}
 		while (computerGames > 0);
 	}
+	
+	public static void addWins()
+	{
+		if (game.isPlayerOneTurn())
+		{
+			if (game.getPlayerOne().isComputer())
+			{
+				game.getPlayerOne().addComputerWin();
+			}
+			else
+			{
+				game.getPlayerOne().addWin();
+			}
+		}
+		else
+		{
+			if (game.getPlayerTwo().isComputer())
+			{
+				game.getPlayerTwo().addComputerWin();
+			}
+			else
+			{
+				game.getPlayerTwo().addWin();
+			}
+		}
+	}
 
+	public static void getComputerGameCount()
+	{
+		System.out.println("How many games would you like the computers to play?");
+		input = scan.nextLine();
+		try
+		{
+			computerGames = Integer.parseInt(input);
+		}
+		catch (Exception e)
+		{
+			try
+			{
+				if ("k".endsWith(input.toLowerCase()))
+				{
+					input = input.substring(0, input.length() - 1);
+					computerGames = Integer.parseInt(input);
+					computerGames *= 1000;
+				}
+				else if ("m".endsWith(input.toLowerCase()))
+				{
+					input = input.substring(0, input.length() - 1);
+					computerGames = Integer.parseInt(input);
+					computerGames *= 1000000;
+				}
+				else
+				{
+					System.out.println("Invalid input, defaulting to 1");
+					computerGames = 1;
+				}
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Invalid input, defaulting to 1");
+				computerGames = 1;
+			}
+		}
+	}
+	
 	public static void printGame()
 	{
 		System.out.print("1 \t");
@@ -251,8 +238,8 @@ public class Nim
 				System.out.println("3");
 			}
 			String row = scan.nextLine();
-			if ("1".equals(row) && Game.getCurrentState().getOne() > 0 || "2".equals(row) && Game.getCurrentState().getTwo() > 0 || 
-					"3".equals(row) && Game.getCurrentState().getThree() > 0)
+			if ("1".equals(row) && Game.getCurrentState().getOne() > 0 || "2".equals(row) && Game.getCurrentState().getTwo() > 0 || "3".equals(row)
+					&& Game.getCurrentState().getThree() > 0)
 			{
 				return row;
 			}
@@ -289,102 +276,5 @@ public class Nim
 				System.out.println("Invalid input, please try again.");
 			}
 		}
-	}
-
-	
-	public static Scanner getScan() {
-		return scan;
-	}
-
-	public static void setScan(Scanner scan) {
-		Nim.scan = scan;
-	}
-
-	public static Game getGame() {
-		return game;
-	}
-
-	public static void setGame(Game game) {
-		Nim.game = game;
-	}
-
-	public static String getRow() {
-		return row;
-	}
-
-	public static void setRow(String row) {
-		Nim.row = row;
-	}
-
-	public static String getAmount() {
-		return amount;
-	}
-
-	public static void setAmount(String amount) {
-		Nim.amount = amount;
-	}
-
-	public static String getInput() {
-		return input;
-	}
-
-	public static void setInput(String input) {
-		Nim.input = input;
-	}
-
-	public static int getComputerGames() {
-		return computerGames;
-	}
-
-	public static void setComputerGames(int computerGames) {
-		Nim.computerGames = computerGames;
-	}
-
-	public static int getPlayerOneWins() {
-		return PlayerOneWins;
-	}
-
-	public static void setPlayerOneWins(int playerOneWins) {
-		PlayerOneWins = playerOneWins;
-	}
-
-	public static int getPlayerTwoWins() {
-		return PlayerTwoWins;
-	}
-
-	public static void setPlayerTwoWins(int playerTwoWins) {
-		PlayerTwoWins = playerTwoWins;
-	}
-
-	public static int getPlayerOneComputerWins() {
-		return PlayerOneComputerWins;
-	}
-
-	public static void setPlayerOneComputerWins(int playerOneComputerWins) {
-		PlayerOneComputerWins = playerOneComputerWins;
-	}
-
-	public static int getPlayerTwoComputerWins() {
-		return PlayerTwoComputerWins;
-	}
-
-	public static void setPlayerTwoComputerWins(int playerTwoComputerWins) {
-		PlayerTwoComputerWins = playerTwoComputerWins;
-	}
-
-	public static boolean isDebugComputers() {
-		return debugComputers;
-	}
-
-	public static void setDebugComputers(boolean debugComputers) {
-		Nim.debugComputers = debugComputers;
-	}
-
-	public static boolean isPrintComputerWins() {
-		return printComputerWins;
-	}
-
-	public static void setPrintComputerWins(boolean printComputerWins) {
-		Nim.printComputerWins = printComputerWins;
 	}
 }
